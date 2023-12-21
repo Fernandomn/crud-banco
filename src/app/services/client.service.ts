2ecb171bf5d0f1b9548e22ae515dcb5f557457cf
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2023-12-21 16:09:32 -0300

    updating ClientService to do all CRUD methods

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -1,11 +1,54 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Client } from '../types/client';
+import { RequestParams } from '../types/common';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ClientService {
-  private readonly API = 'http://localhost:3000/pensamentos'
+  private readonly clientsApiUrl = 'http://localhost:3000/clients';
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {}
+
+  createClient(client: Client): Observable<Client> {
+    return this.http.post<Client>(this.clientsApiUrl, client);
+  }
+
+  listClients(requestParams?: RequestParams): Observable<Client[]> {
+    const itensPerPage = 10;
+    let params = new HttpParams();
+    //   .set('_page', page)
+    //   .set('_limit', itensPerPage);
+
+    if (requestParams) {
+      params = params
+        .set('_page', requestParams._page)
+        .set('_limit', itensPerPage);
+      if (requestParams.filter && requestParams.filter?.trim().length > 0) {
+        params = params.set('q', requestParams.filter);
+      }
+    }
+
+    return this.http.get<Client[]>(this.clientsApiUrl, { params });
+  }
+
+  getClientById(id: string): Observable<Client> {
+    const url = `${this.clientsApiUrl}/${id}`;
+
+    return this.http.get<Client>(url);
+  }
+
+  editClient(client: Client): Observable<Client> {
+    const url = `${this.clientsApiUrl}/${client.id}`;
+
+    return this.http.put<Client>(url, client);
+  }
+
+  deleteClient(id: string): Observable<Client> {
+    const url = `${this.clientsApiUrl}/${id}`;
+
+    return this.http.delete<Client>(url);
+  }
 }
