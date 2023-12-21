1cfb13dcc235b4687ae45e5f79edfcae3a04e2de
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2023-12-21 01:08:50 -0300

    adding styles. creating header component. creating logo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientsListComponent } from './components/clients-list/clients-list.component';
+import { ClientsFormComponent } from './components/clients-form/clients-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'list-clients', pathMatch: 'full' },
+  { path: 'list-clients', component: ClientsListComponent },
+  { path: 'create-clients', component: ClientsFormComponent },
+  { path: 'edit-clients/:id', component: ClientsFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClientsFormComponent } from './components/clients-form/clients-form.component';
 import { ClientsListComponent } from './components/clients-list/clients-list.component';
+import { HeaderComponent } from './components/common/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, ClientsListComponent, ClientsFormComponent],
+  declarations: [AppComponent, ClientsListComponent, ClientsFormComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
